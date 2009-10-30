9f20e2d28dd6afe08296bd0e4bce6924864fd36b
Mike Skalnik <mike.skalnik@gmail.com>
2009-10-30 18:28:33 -0400

    Added github lib

---
+++ lib/github.rb
@@ -0,0 +1,35 @@
+require 'simple-rss'
+require 'open-uri'
+
+helpers do
+  def github_activity(username)
+    url = "http://github.com/#{username}.atom"
+    activity_feed = SimpleRSS.parse open(url)
+    activity = []
+    activity_feed.items.each do |act|
+      formatted_act = {:title => act.title, :content => unescape(act.content), :link => act.link,
+                       :source => 'Github', :published => act.published}
+      activity << formatted_act
+    end
+    return activity.reverse
+  end
+  
+  def unescape(string)
+    return_string = string.gsub('&lt;', '<')
+    return_string.gsub!('&gt;', '>')
+    return_string.gsub!('&quot;', '"')
+    return return_string
+  end
+end
+
+get '/fetch_github_activity' do
+  activity = fetch_github_activity
+  activity.each do |act|
+    unless Entry.count(act) > 0
+      entry = Entry.new(act)
+      entry.save!
+      sleep 1 
+    end
+  end
+  redirect '/'
+end
