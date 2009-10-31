002e64edd06d603088a4e460ddbdb5854839bfff
Mike Skalnik <mike.skalnik@gmail.com>
2009-10-30 23:35:44 -0400

    Added general lib, which adds a /fetch_all

---
+++ lib/general.rb
@@ -0,0 +1,12 @@
+get '/fetch_all' do
+  last_fm = fetch_last_fm_tracks
+  github = fetch_github_activity
+  twitter = fetch_tweets
+  all = last_fm + github + twitter
+  all.each do |new_entry|
+    unless Entry.count(new_entry) > 0
+      entry = Entry.new(new_entry)
+      entry.save!
+    end
+  end
+end
